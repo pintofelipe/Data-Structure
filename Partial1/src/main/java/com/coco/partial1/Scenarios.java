d80653b87a3cdec893f0835e8573c18f6ef83148
pintofelipe <ufpintou@ufpso.edu.co>
2024-03-27 20:04:28 -0500

    add funcionalidad de escenario más visitado

--- Partial1/src/main/java/com/coco/partial1/Scenarios.java
+++ Partial1/src/main/java/com/coco/partial1/Scenarios.java
@@ -3,6 +3,7 @@ package com.coco.partial1;
 
 import java.util.List;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 public final class Scenarios extends javax.swing.JFrame {
 
@@ -12,7 +13,7 @@ public final class Scenarios extends javax.swing.JFrame {
 
 
     List<List<Integer>> datos = new ArrayList<>();
-    
+    String[] nombresEscenarios = {"Mini pista patinaje", "Piscina olímpica", "Estadio", "Parque infantil"};
     
     
     
@@ -27,6 +28,8 @@ public final class Scenarios extends javax.swing.JFrame {
     private void initComponents() {
 
         jbMatriz = new javax.swing.JButton();
+        jbEscenariosMasVisitados = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -37,21 +40,46 @@ public final class Scenarios extends javax.swing.JFrame {
             }
         });
 
+        jbEscenariosMasVisitados.setText("Escenario más visitado");
+        jbEscenariosMasVisitados.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbEscenariosMasVisitadosActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(153, 102, 255));
+        jLabel1.setText("Escenarios Visitados en Ocaña");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(72, 72, 72)
-                .addComponent(jbMatriz)
-                .addContainerGap(561, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(197, 197, 197)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(38, 38, 38)
+                        .addComponent(jbEscenariosMasVisitados, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(139, 139, 139)
+                        .addComponent(jbMatriz)))
+                .addContainerGap(219, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(62, 62, 62)
-                .addComponent(jbMatriz)
-                .addContainerGap(375, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(jLabel1)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(jbMatriz, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(48, 48, 48)
+                        .addComponent(jbEscenariosMasVisitados)))
+                .addContainerGap(371, Short.MAX_VALUE))
         );
 
         pack();
@@ -60,6 +88,38 @@ public final class Scenarios extends javax.swing.JFrame {
     private void jbMatrizActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbMatrizActionPerformed
         mostrarMatriz(datos);
     }//GEN-LAST:event_jbMatrizActionPerformed
+
+    private void jbEscenariosMasVisitadosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbEscenariosMasVisitadosActionPerformed
+        
+        int filaMasVisitada =0;
+        int maxVisitas =0;
+        
+        for(int i=0; i<datos.size();i++){
+            List<Integer> fila = datos.get(i); // obtiene una fila la primera Va ha ir comparando
+            
+            
+            int totalDeVisitas = 0;
+            
+            for (int visitas: fila) {
+                totalDeVisitas+=visitas;
+            }
+            
+            
+            
+            if(totalDeVisitas>maxVisitas){
+                maxVisitas = totalDeVisitas;
+                filaMasVisitada = i;
+            }
+            
+        }
+        
+ 
+        String escenarioMasVisitado = nombresEscenarios[filaMasVisitada];
+        
+        JOptionPane.showMessageDialog(null, "El escenario más visitado durante la semana es "+escenarioMasVisitado);
+        
+        
+    }//GEN-LAST:event_jbEscenariosMasVisitadosActionPerformed
 
     public static void main(String args[]) {
         
@@ -109,6 +169,8 @@ public final class Scenarios extends javax.swing.JFrame {
         }
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton jbEscenariosMasVisitados;
     private javax.swing.JButton jbMatriz;
     // End of variables declaration//GEN-END:variables
 }
