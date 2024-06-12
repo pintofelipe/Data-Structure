7f3ca4ac346cac25c1c5e3870fc1ec5c1b98d420
pintofelipe <afpintou@ufpso.edu.co>
2024-06-11 23:09:13 -0500

    add limpiar table

--- ExamenFinal/Hospital/src/main/java/com/coco77/hospital/Main.java
+++ ExamenFinal/Hospital/src/main/java/com/coco77/hospital/Main.java
@@ -9,6 +9,8 @@ import javax.swing.table.DefaultTableModel;
 public class Main extends javax.swing.JFrame {
      private static List<Usuario> usuarios = new ArrayList<>();
      DefaultTableModel modelo;
+     private static LinkedList<Usuario> colaPreferencial = new LinkedList<>();
+    private static LinkedList<Usuario> colaNoPreferencial = new LinkedList<>();
      
     
     public Main() {
@@ -43,8 +45,43 @@ public class Main extends javax.swing.JFrame {
         }
     }
     
+        private void solicitarTurno() {
+        String documento = JOptionPane.showInputDialog(this, "Ingrese el documento:");
+        if (documento != null && !documento.trim().isEmpty()) {
+            boolean encontrado = false;
+            for (Usuario usuario : usuarios) {
+                if (usuario.getDocumento().equals(documento)) {
+                    colaPreferencial.add(usuario);
+                    encontrado = true;
+                    JOptionPane.showMessageDialog(this, "Turno asignado en la cola preferencial.");
+                    break;
+                }
+            }
+            if (!encontrado) {
+                Usuario nuevoUsuario = new Usuario(documento, "No registrado", "Desconocido", "Desconocido", "Desconocido", 0, 0);
+                colaNoPreferencial.add(nuevoUsuario);
+                JOptionPane.showMessageDialog(this, "Usuario no registrado. Turno asignado en la cola no preferencial.");
+            }
+        } else {
+            JOptionPane.showMessageDialog(this, "Documento inválido.");
+        }
+    }
     
     
+    private void mostrarColas() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cola Preferencial:\n");
+        for (Usuario usuario : colaPreferencial) {
+            sb.append(usuario.getNombre()).append(" (").append(usuario.getDocumento()).append(")\n");
+        }
+
+        sb.append("\nCola No Preferencial:\n");
+        for (Usuario usuario : colaNoPreferencial) {
+            sb.append(usuario.getNombre()).append(" (").append(usuario.getDocumento()).append(")\n");
+        }
+
+        JOptionPane.showMessageDialog(this, sb.toString());
+    }
    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -54,6 +91,9 @@ public class Main extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jtListaPersonas = new javax.swing.JTable();
         jbConsultarTurnos = new javax.swing.JButton();
+        jbConsultarTurnos1 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jbConsultarTurnos2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -80,42 +120,93 @@ public class Main extends javax.swing.JFrame {
             }
         });
 
+        jbConsultarTurnos1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jbConsultarTurnos1.setForeground(new java.awt.Color(102, 102, 255));
+        jbConsultarTurnos1.setText("Solicitar Turno");
+        jbConsultarTurnos1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(51, 51, 255), 1, true));
+        jbConsultarTurnos1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbConsultarTurnos1ActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setFont(new java.awt.Font("sansserif", 1, 18)); // NOI18N
+        jLabel2.setText("Solicitud de Turnos");
+
+        jbConsultarTurnos2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jbConsultarTurnos2.setForeground(new java.awt.Color(102, 102, 255));
+        jbConsultarTurnos2.setText("Consultar Colas");
+        jbConsultarTurnos2.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(51, 51, 255), 1, true));
+        jbConsultarTurnos2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbConsultarTurnos2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(41, 41, 41)
+                        .addComponent(jbConsultarTurnos1, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(54, 54, 54)
+                        .addComponent(jbConsultarTurnos2, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(75, 75, 75)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 196, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 67, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 288, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(218, 218, 218))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(34, 34, 34)
                         .addComponent(jbConsultarTurnos, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(54, 54, 54))))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(83, 443, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 670, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                        .addGap(54, 54, 54))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 670, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(29, 29, 29))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(14, 14, 14)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jbConsultarTurnos, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 26, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 383, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(68, 68, 68))
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(19, 19, 19)
+                                .addComponent(jbConsultarTurnos, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(30, 30, 30)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 383, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addGap(55, 55, 55)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jbConsultarTurnos1, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jbConsultarTurnos2, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(107, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jbConsultarTurnosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbConsultarTurnosActionPerformed
+        limpiarTabla();
         listar();
     }//GEN-LAST:event_jbConsultarTurnosActionPerformed
+
+    private void jbConsultarTurnos1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbConsultarTurnos1ActionPerformed
+        solicitarTurno();
+        limpiarTabla();
+        listar();
+    }//GEN-LAST:event_jbConsultarTurnos1ActionPerformed
+
+    private void jbConsultarTurnos2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbConsultarTurnos2ActionPerformed
+        mostrarColas();
+    }//GEN-LAST:event_jbConsultarTurnos2ActionPerformed
 
     public static void main(String args[]) {
         //Ajustar la ruta del archivo txt
@@ -165,8 +256,11 @@ public class Main extends javax.swing.JFrame {
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton jbConsultarTurnos;
+    private javax.swing.JButton jbConsultarTurnos1;
+    private javax.swing.JButton jbConsultarTurnos2;
     private javax.swing.JTable jtListaPersonas;
     // End of variables declaration//GEN-END:variables
 }
